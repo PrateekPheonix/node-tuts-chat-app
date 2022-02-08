a1688d75c0d6b06cffe68f38e50ec3fe6d806748
Prateek Niket <prateekniket25@gmail.com>
2022-02-08 22:33:42 +0530

    added from state

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,29 @@
 const socket = io()
 
+// Elements
+const messageForm = document.querySelector('#message-form')
+const messageFormInput = messageForm.querySelector('input')
+const messageFormButton = messageForm.querySelector('button')
+const sendLocationButton = document.querySelector('#send-location')
+
 socket.on('showMessage', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    // disable
+    messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message, (err) => {
+        // enable
+        messageFormButton.removeAttribute('disabled')
+        messageFormInput.value = ''
+        messageFormInput.focus()
+
         if (err) {
             return console.log(err)
         }
@@ -16,16 +31,21 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported')
     }
+
+    // disable
+    sendLocationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            // enable
+            sendLocationButton.removeAttribute('disabled')
             console.log("Location shared")
         })
     })
